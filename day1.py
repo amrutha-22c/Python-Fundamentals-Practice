0b379f87f3107b5fe32668ff2f6f21eb33d23c3b
amrutha-22c <amrutha9763@gmail.com>
2026-03-17 10:33:00 +0530

    Add three programs for addition, even/odd, and factorial

--- day1.py
+++ day1.py
@@ -1,1 +1,20 @@
+# Program 1: Addition of two numbers
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+print("Sum:", a + b)
 
+
+# Program 2: Even or Odd
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+# Program 3: Factorial
+n = int(input("Enter number: "))
+fact = 1
+for i in range(1, n+1):
+    fact *= i
+print("Factorial:", fact)
